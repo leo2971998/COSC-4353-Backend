fec4b65fd5bdc86614eb894e2b6387d658797f9a
Anh Tu Nguyen <106453722+leo2971998@users.noreply.github.com>
2025-07-31 22:06:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ import cors from "cors";
 import mysql from "mysql2/promise";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
-import eventRoutes from "./routes/eventRoutes.js";
-import matchRoutes from "./routes/match.js";
-import notificationRoutes from './routes/notifications.js';
-import historyRoutes from "./routes/historyRoutes.js";
 dotenv.config();
 
 const app = express();
